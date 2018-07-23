4ab5daf3607dd29b16aff5ff67d14f8400f5e595
NIKITA AD <nikita8ad@gmail.com>
2018-07-23 08:47:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your functions and code here
+kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens
+}
