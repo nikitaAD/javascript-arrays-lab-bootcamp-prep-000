891be247d8bb999d6cf262483220745bd20238c6
NIKITA AD <nikita8ad@gmail.com>
2018-07-23 09:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,12 +23,10 @@ function appendKitten(name){
 
 function prependKitten(name){
   return [name, ...kittens];
- // return newArr;
 }
 
 function removeLastKitten(){
    return kittens.slice(0,kittens.length -1);
-  // return newArr;
 }
 
 function removeFirstKitten(){
